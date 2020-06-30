ee2ac0892a7822f556b7401ccadcf182f94398f8
Maayan_Belzer <maayan.belzer@gmail.com>
2020-06-30 12:35:59 -0700

    initial traffic light

---
+++ traffic_light.erl
@@ -0,0 +1,208 @@
+%%%-------------------------------------------------------------------
+%%% @author maayan
+%%% @copyright (C) 2020, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 30. Jun 2020 8:32 AM
+%%%-------------------------------------------------------------------
+-module(traffic_light).
+-author("maayan").
+
+-behaviour(gen_statem).
+
+%% API
+-export([start_link/0]).
+
+%% gen_statem callbacks
+-export([
+  init/1,
+  format_status/2,
+  state_name/3,
+  handle_event/4,
+  terminate/3,
+  code_change/4,
+  callback_mode/0
+]).
+%Events
+-export([timeout/0,server_msg/0]).
+
+%States
+-export([red/3,green/3,yellow/3]).
+
+-define(SERVER, ?MODULE).
+
+-record(traffic_light_state, {}).
+
+%%%===================================================================
+%%% API
+%%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Creates a gen_statem process which calls Module:init/1 to
+%% initialize. To ensure a synchronized start-up procedure, this
+%% function does not return until Module:init/1 has returned.
+%%
+%% @spec start_link() -> {ok, Pid} | ignore | {error, Error}
+%% @end
+%%--------------------------------------------------------------------
+start_link() ->
+  gen_statem:start_link({local, ?SERVER}, ?MODULE, [], []).
+
+%%%===================================================================
+%%% gen_statem callbacks
+%%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Whenever a gen_statem is started using gen_statem:start/[3,4] or
+%% gen_statem:start_link/[3,4], this function is called by the new
+%% process to initialize.
+%%
+%% @spec init(Args) -> {CallbackMode, StateName, State} |
+%%                     {CallbackMode, StateName, State, Actions} |
+%%                     ignore |
+%%                     {stop, StopReason}
+%% @end
+%%--------------------------------------------------------------------
+init([]) ->
+  {ok, state_name, #traffic_light_state{}}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% This function is called by a gen_statem when it needs to find out 
+%% the callback mode of the callback module.
+%%
+%% @spec callback_mode() -> atom().
+%% @end
+%%--------------------------------------------------------------------
+callback_mode() ->
+  state_functions.
+
+%%Events
+timeout() -> gen_statem:cast(?MODULE,{time}).
+server_msg() -> gen_statem:cast(?MODULE,{msg}).
+
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Called (1) whenever sys:get_status/1,2 is called by gen_statem or
+%% (2) when gen_statem terminates abnormally.
+%% This callback is optional.
+%%
+%% @spec format_status(Opt, [PDict, StateName, State]) -> term()
+%% @end
+%%--------------------------------------------------------------------
+format_status(_Opt, [_PDict, _StateName, _State]) ->
+  Status = some_term,
+  Status.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% There should be one instance of this function for each possible
+%% state name.  If callback_mode is statefunctions, one of these
+%% functions is called when gen_statem receives and event from
+%% call/2, cast/2, or as a normal process message.
+%%
+%% @spec state_name(Event, From, State) ->
+%%                   {next_state, NextStateName, NextState} |
+%%                   {next_state, NextStateName, NextState, Actions} |
+%%                   {stop, Reason, NewState} |
+%%    				 stop |
+%%                   {stop, Reason :: term()} |
+%%                   {stop, Reason :: term(), NewData :: data()} |
+%%                   {stop_and_reply, Reason, Replies} |
+%%                   {stop_and_reply, Reason, Replies, NewState} |
+%%                   {keep_state, NewData :: data()} |
+%%                   {keep_state, NewState, Actions} |
+%%                   keep_state_and_data |
+%%                   {keep_state_and_data, Actions}
+%% @end
+%%--------------------------------------------------------------------
+state_name(_EventType, _EventContent, State) ->
+  NextStateName = next_state,
+  {next_state, NextStateName, State}.
+
+
+red(cast,{time},State = #traffic_light_state{}) ->
+  % TODO: turn green
+  NextStateName = green,
+  {next_state, NextStateName, State};
+red(cast,{msg},State = #traffic_light_state{}) ->
+  % TODO: turn green
+  NextStateName = green,
+  {next_state, NextStateName, State}.
+yellow(cast,{time},State = #traffic_light_state{}) ->
+  % TODO: turn red
+  NextStateName = red,
+  {next_state, NextStateName, State}.
+green(cast,{time},State = #traffic_light_state{}) ->
+  % TODO: turn yellow
+  NextStateName = yellow,
+  {next_state, NextStateName, State};
+green(cast,{msg},State = #traffic_light_state{}) ->
+  % TODO: turn yellow
+  NextStateName = yellow,
+  {next_state, NextStateName, State}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%%
+%% If callback_mode is handle_event_function, then whenever a
+%% gen_statem receives an event from call/2, cast/2, or as a normal
+%% process message, this function is called.
+%%
+%% @spec handle_event(Event, StateName, State) ->
+%%                   {next_state, NextStateName, NextState} |
+%%                   {next_state, NextStateName, NextState, Actions} |
+%%                   {stop, Reason, NewState} |
+%%    				 stop |
+%%                   {stop, Reason :: term()} |
+%%                   {stop, Reason :: term(), NewData :: data()} |
+%%                   {stop_and_reply, Reason, Replies} |
+%%                   {stop_and_reply, Reason, Replies, NewState} |
+%%                   {keep_state, NewData :: data()} |
+%%                   {keep_state, NewState, Actions} |
+%%                   keep_state_and_data |
+%%                   {keep_state_and_data, Actions}
+%% @end
+%%--------------------------------------------------------------------
+handle_event(_EventType, _EventContent, _StateName, State) ->
+  NextStateName = the_next_state_name,
+  {next_state, NextStateName, State}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% This function is called by a gen_statem when it is about to
+%% terminate. It should be the opposite of Module:init/1 and do any
+%% necessary cleaning up. When it returns, the gen_statem terminates with
+%% Reason. The return value is ignored.
+%%
+%% @spec terminate(Reason, StateName, State) -> void()
+%% @end
+%%--------------------------------------------------------------------
+terminate(_Reason, _StateName, _State) ->
+  ok.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Convert process state when code is changed
+%%
+%% @spec code_change(OldVsn, StateName, State, Extra) ->
+%%                   {ok, StateName, NewState}
+%% @end
+%%--------------------------------------------------------------------
+code_change(_OldVsn, StateName, State, _Extra) ->
+  {ok, StateName, State}.
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
